bd8395f1f52828673d57f9b0d6f36358e3ce8e35
Adrian Buzatu <adrian.buzatu@gameloft.com>
2018-06-08 18:34:34 +0300

    work

--- src/App.js
+++ src/App.js
@@ -7,7 +7,39 @@ import Filter from './components/Filter';
 
 let fakeServerData = {
   user: {
-    name: 'Adrian'
+    name: 'Adrian',
+    playlists: [
+      {
+        id: 1,
+        name: "Feeling Good Friday",
+        songs: ['Fake Love', 'Canelloni Makaroni', 'New Light'],
+        image: "https://i.scdn.co/image/0a44b70f4fc5fb993a2efd299a73f0e24c21a775"
+      },
+      {
+        id: 2,
+        name: "Top 50 #Romania",
+        songs: ['Lucid Dreams', 'Taste', 'Solo'],
+        image: "https://charts-images.scdn.co/REGIONAL_RO_DEFAULT.jpg"
+      },
+      {
+        id: 3,
+        name: "Have A Great Day",
+        songs: ['Welcome to the Party', 'Back To You', 'Start Again'],
+        image: "https://i.scdn.co/image/954f97a75b44fb3e6ca974cd46eb58989f205a96"
+      },    
+      {
+        id: 4,
+        name: "Wake Up Happy",
+        songs: ['Rise', 'Nervous', 'Looking Back Someday'],
+        image: "https://i.scdn.co/image/237dbe5ad6d3e8a60b0d79d5a6303cc531b56440"
+      },
+      {
+        id: 5,
+        name: "Rock This",
+        songs: ['Solara', 'The Rover', 'Blood // Water'],
+        image: "https://i.scdn.co/image/e4e0c950e8015d6942795b708398d6d3c2c189c9"
+      },                
+    ]
   }
 }
 class App extends Component {
@@ -20,15 +52,20 @@ class App extends Component {
       <Summary/>
 
         <div style={displayComponent}>
-          <Playlist playListName="Playlist 01"/>
-          <Playlist playListName="Playlist 02"/>
-          <Playlist playListName="Playlist 03"/>
-          <Playlist playListName="Playlist 04"/>
-          <Playlist playListName="Playlist 05"/>
-          
+
+          {
+            this.getPlayLists()
+          }
         </div>
       </div>
     );
+  }
+
+  getPlayLists() {
+      const playLists = fakeServerData.user.playlists.map((obj) =>
+        <Playlist key={obj.id} playListName={obj.name} image={obj.image} songs={obj.songs}/>
+      );
+      return playLists;
   }
 }
 
--- src/components/Playlist.js
+++ src/components/Playlist.js
@@ -8,21 +8,43 @@ class Playlist extends Component {
       }
 
     render() {
-        const defaultTextColor = '#fff';
-        let styleComponent = {"width" : "1fr", "height": "auto", "backgroundColor": "#ddd", "alignContent": "spaceAround", "margin": "5px", "color": defaultTextColor};
-        let imgComponent = {"width" : "214px", "height": "214px", "borderRadius":"100px"};
+        const defaultTextColor = 'black';
+        let styleComponent = {
+            "width" : "1fr", 
+            "height": "auto",
+            "backgroundColor": "#ddd", 
+            "alignContent": "spaceAround", 
+            "margin": "5px", 
+            "color": defaultTextColor};
+        let imgComponent = {
+            "width" : "214px", 
+            "height": "214px",
+            "borderRadius":"0px"
+        };
+
+        let songsStyle = {
+            "listStyleType": "none",
+            "textAlign": "left"
+        }
 
         return(
             <div style={styleComponent}>
-                <img src="https://i.scdn.co/image/3b7e8199da16b8d83cd2323a3abd8b700248aea8" style={imgComponent} alt="ce smart e, nu pot folosi same text"/>
+                <img src={this.props.image} style={imgComponent} alt={this.props.playListName}/>
                 <h3> {this.props.playListName} </h3>
-                <ul>
-                    <li> Song 01</li>
-                    <li> Song 02</li>
-                    <li> Song 03</li>
+                <ul style={songsStyle}>
+                    {this.getSongs()}
                 </ul>
             </div>
         );
+    }
+
+    getSongs() {
+        
+        const songList = this.props.songs.map((obj) =>
+            <li>{obj}</li>
+        );
+
+        return songList;
     }
 }
 export default Playlist;
